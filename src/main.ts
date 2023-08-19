56900e6eba7cf4b37c2d161ef8870c8acd75a966
Lucas Henneke <70382336+zweek@users.noreply.github.com>
2023-08-19 12:20:51 +0200

    remove settings tab
    not needed imo opinion

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,15 @@
 import { EnhancedFootnote } from "./footnote"
 import {
-	App,
 	Editor,
 	MarkdownView,
 	Plugin,
-	PluginSettingTab,
-	Setting
 } from 'obsidian';
 
-interface EnhancedFootnotesSettings {
-	treatFootnotesAsComments: boolean;
-}
-
-const DEFAULT_SETTINGS: EnhancedFootnotesSettings = {
-	treatFootnotesAsComments: true,
-}
-
 export default class EnhancedFootnotesPlugin extends Plugin {
-	settings: EnhancedFootnotesSettings;
 	
 	private footnote: EnhancedFootnote;
 
 	async onload() {
-		await this.loadSettings();
-
 		this.footnote = new EnhancedFootnote()
 
 		this.addCommand({
@@ -43,47 +29,9 @@ export default class EnhancedFootnotesPlugin extends Plugin {
 				this.footnote.AddNamedFootnote(editor)
 			}
 		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: EnhancedFootnotesPlugin;
-
-	constructor(app: App, plugin: EnhancedFootnotesPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Treat footnotes as comments')
-			.setDesc('When adding a footnote to a text selection, add a highlight the selected text')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.treatFootnotesAsComments)
-				.onChange(async (value) => {
-					this.plugin.settings.treatFootnotesAsComments = value;
-					await this.plugin.saveSettings();
-				}));
 
 	}
 }
